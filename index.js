307dfa101a27f7cb02ff69aec94e424e19ea1822
CLII412 <sw412fr@gmail.com>
2023-06-15 22:37:11 +0300

    upd

--- index.js
+++ index.js
@@ -1,138 +1,197 @@
-// Задача 1: Напишіть функцію, яка перетворює рядок в число, використовуючи Number()
-
-function stringToNumber(str) {
-  // Використовуємо Number() для перетворення рядка в число
-  const number = Number(str);
-
-  // Повертаємо отримане число
-  return number;
+//Завдання 1: Функція для створення символу
+function createSymbol() {
+  // Створюємо символ без опису
+  return Symbol();
 }
 
 console.log("Завдання 1 ====================================");
-console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
+let symbol1 = createSymbol(); // Створюємо новий символ
+console.log(symbol1); // Виведе символ
 
-// Задача 2: Напишіть функцію, яка визначає, чи є два числа майже однаковими, використовуючи Number.EPSILON
-
-function isAlmostSame(num1, num2) {
-  // Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
-  return Math.abs(num1 - num2) <= Number.EPSILON;
+//Завдання 2: Функція для створення символу з описом та виведення опису
+function createSymbolWithDescription() {
+  // Створюємо символ symbol з описом 'My symbol'
+  let symbol = Symbol("My symbol");
+  //Повертаємо опис символа symbol
+  return symbol.description;
 }
 
 console.log("Завдання 2 ====================================");
-console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // Виведе true
+console.log(createSymbolWithDescription); // Виводить опис символу
 
-// Задача 3: Напишіть функцію, яка перевіряє, чи є число безпечним цілим числом
+// Завдання 3: Створення властивостей об'єкту з використанням Symbol
+// В даному завданні ми створимо властивість об'єкту, ключ якої буде символом.
 
-function isSafeInteger(num) {
-  // Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
-  return num <= Number.MAX_SAFE_INTEGER && num >= Number.MIN_SAFE_INTEGER;
+// Створимо функцію, яка створює об'єкт з властивістю, ключ якої є символом
+function createSymbolProperty() {
+  // Створимо змінну sym яка дорівнює символу з описом "property"
+  let sym = Symbol("property");
+
+  // Створимо пустий об'єкт
+  let obj = {};
+
+  // Додамо властивість до об'єкту, ключем якої буде наш символ, а значенням буде рядок Це властивість об'єкту з використанням символу
+  obj[sym] = "Це властивість об'єкту з використанням символу";
+
+  // Повертаємо об'єкт
+  return obj;
+}
+
+// Створимо функцію, яка виводить значення властивості об'єкту по ключу-символу
+function printSymbolProperty(obj, sym) {
+  // Виводимо значення властивості об'єкту по ключу-символу
+  console.log(obj[sym]);
 }
 
 console.log("Завдання 3 ====================================");
-console.log(
-  "isSafeInteger(Number.MAX_SAFE_INTEGER + 1)",
-  isSafeInteger(Number.MAX_SAFE_INTEGER + 1)
-); // Виведе false
 
-// Задача 4: Використовуючи результати Задачі 3, напишіть функцію, яка перевіряє, чи є число небезпечним цілим числом
-
-function isUnsafeInteger(num) {
-  // Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
-  return !isSafeInteger(num);
+// Завдання 4. Перевіримо чи є символи унікальними
+// Функція для створення двох символів з однаковим описом та порівнянням
+function createAndCompareTwoSymbols(value) {
+  // Створюємо два символи з однаковим описом value, та повертаємо результат порівняння двох символів
+  return Symbol(value) === Symbol(value);
 }
 
 console.log("Завдання 4 ====================================");
-console.log(
-  "isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)",
-  isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)
-); // Виведе true
 
-// Задача 5: Напишіть функцію, яка перевіряє, чи є число надто великим для представлення в JavaScript
+console.log(createAndCompareTwoSymbols("My Symbol")); // Виведе false, оскільки кожен символ є унікальним
 
-function isTooLarge(num) {
-  // Перевіряємо, чи є число більшим за MAX_VALUE
-  return num > Number.MAX_VALUE;
+// Завдання 5: Створення символу з використанням Symbol.for()
+
+// Функція createSymbolForKey створює новий символ за допомогою Symbol.for() та повертає його
+function createSymbolForKey(key) {
+  // Використовуємо Symbol.for() для створення нового символу symbol зі значенням key
+  // Symbol.for() шукає символ в глобальному реєстру символів. Якщо символ з таким ключем вже існує, він повертає його, в іншому випадку створює новий символ з даним ключем та додає його в реєстр
+  let symbol = Symbol.for(key);
+
+  // Повертаємо створений символ
+  return symbol;
+}
+
+// Функція compareSymbolsForKey порівнює два символи за допомогою оператора '==='
+function compareSymbolsForKey(symbol1, symbol2) {
+  // Використовуємо оператор '===' для порівняння двох символів
+  // Якщо два символи однакові, він поверне true, в іншому випадку - false
+  return symbol1 === symbol2;
 }
 
 console.log("Завдання 5 ====================================");
-console.log(
-  "isTooLarge(Number.MAX_VALUE * 2)",
-  isTooLarge(Number.MAX_VALUE * 2)
-); // Виведе true
+let symbol5 = createSymbolForKey("myKey"); // Створюємо новий символ або отримуємо існуючий з глобального реєстру символів
+let symbol6 = createSymbolForKey("myKey"); // Створюємо новий символ або отримуємо існуючий з глобального реєстру символів
+console.log(compareSymbolsForKey(symbol5, symbol6)); //Виведе: true
 
-// Задача 6: Напишіть функцію, яка перевіряє, чи є число меншим чим  найменше можливе числове значення в JavaScript
+// Завдання 6: Використання Symbol.keyFor(sym)
 
-function isAlmostZero(num) {
-  // Перевіряємо, чи є число більшим за 0, але меншим за MIN_VALUE
-  return num > 0 && num < Number.MIN_VALUE;
+// Функція getSymbolKey приймає символ як аргумент та повертає ключ, який був використаний для його створення в глобальному реєстрі символів
+function getSymbolKey(symbol) {
+  // Використовуємо Symbol.keyFor(sym) для отримання ключа, який був використаний для створення символу в глобальному реєстрі символів
+  // Symbol.keyFor(sym) шукає символ в глобальному реєстрі символів та повертає його ключ, якщо він існує. Якщо символ не існує в глобальному реєстрі, він повертає undefined
+  return Symbol.keyFor(symbol);
 }
 
 console.log("Завдання 6 ====================================");
-console.log(
-  "isAlmostZero(Number.MIN_VALUE / 2)",
-  isAlmostZero(Number.MIN_VALUE / 2)
-); // Виведе false
-console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); // Виведе false
+let symbolForKey = Symbol.for("myKey"); // Створюємо новий символ або отримуємо існуючий з глобального реєстру символів
+console.log(getSymbolKey(symbolForKey)); // Виводимо ключ символу. Виведе: "myKey"
 
-// Задача 7: Напишіть функцію, яка перевіряє, чи є значення цілим числом
+// Завдання 7: Використання Symbol.isConcatSpreadable
 
-function checkIsInteger(num) {
-  // Використовуємо вбудовану функцію Number.isInteger(), щоб перевірити, чи є значення цілим числом
-  return Number.isInteger(num);
+// Функція useIsConcatSpreadable використовує властивість Symbol.isConcatSpreadable для вказівки, чи масив розкривається під час конкатенації.
+
+function useIsConcatSpreadable() {
+  // Створюємо перший масив array1 зі значеннями 1, 2, 3.
+  let array1 = [1, 2, 3];
+
+  // Створюємо другий масив array2 зі значеннями 4, 5, 6.
+  let array2 = [4, 5, 6];
+
+  // Встановлюємо властивість Symbol.isConcatSpreadable для масиву array2 в значення false.
+  array2[Symbol.isConcatSpreadable] = false;
+
+  // Використовуємо метод concat для об'єднання масивів array1 і array2.
+  let concatArray = array1.concat(array2);
+
+  // Повертаємо результат конкатенації.
+  return concatArray;
 }
 
 console.log("Завдання 7 ====================================");
-console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
+console.log(useIsConcatSpreadable()); // Виведе [ 1, 2, 3, [ 4, 5, 6, [Symbol(Symbol.isConcatSpreadable)]: false ] ]
 
-// Задача 8: Напишіть функцію, яка перевіряє, чи є значення безпечним цілим числом
+// Завдання 8: Використання Symbol.toString
 
-function checkIsSafeInteger(num) {
-  // Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
-  return Number.isSafeInteger(num);
+function useSymbolToStringTag() {
+  // Створюємо об'єкт myObject.
+  let myObject = {};
+
+  // Встановлюємо властивість Symbol.toStringTag об'єкту myObject на значення "CustomObject".
+  myObject[Symbol] = "CustomObject";
+
+  // Повертаємо рядкове представлення об'єкта myObject[Symbol].
+  return myObject[Symbol].toString();
 }
 
 console.log("Завдання 8 ====================================");
-console.log(
-  "checkIsSafeInteger(Math.pow(2, 53))",
-  checkIsSafeInteger(Math.pow(2, 53))
-); // Виведе false
+console.log(useSymbolToStringTag()); //Виведе CustomObject
 
-// Задача 9: Напишіть функцію, яка конвертує число в рядок з експоненційним представленням
+// Завдання 9: Використання Symbol.description
 
-function convertToExponential(num) {
-  // Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
-  return num.toExponential();
+// Функція useSymbolDescription використовує властивість Symbol.description для отримання опису символу.
+
+function useSymbolDescription() {
+  // Створюємо символ mySymbol зі значенням "mySymbol".
+  let mySymbol = Symbol("mySymbol");
+
+  // Отримуємо опис символу mySymbol в змінну symbolDescription за допомогою властивості description.
+  let symbolDescription = mySymbol.description;
+
+  // Повертаємо symbolDescription
+  return symbolDescription;
 }
 
 console.log("Завдання 9 ====================================");
-console.log("convertToExponential(42)", convertToExponential(42)); // Виведе "4.2e+1"
+console.log(useSymbolDescription()); //Виведе mySymbol
 
-// Задача 10: Напишіть функцію, яка конвертує число в рядок з фіксованою кількістю знаків після коми
+// Завдання 10: Використання Symbol.iterator
 
-function convertToFixed(num, precision) {
-  // Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
-  return num.toFixed(precision);
+// Об'єкт "myObject" представляє значення   from: 1, to: 7, які можна перебрати
+let myObject = {
+  from: 1,
+  to: 7,
+
+  // Використовуємо Symbol.iterator для створення ітератора всередині об'єкта "myObject"
+  [Symbol.iterator]() {
+    // this.current присвоюємо this.from
+    this.current = this.from;
+    return this; // Індекс для ітерації по масиву ключів
+  },
+  //створюжмо метод "next" який визначає поведінку при кожній ітерації
+  next() {
+    if (this.current < this.to) {
+      // Використовуйте if
+      // Якщо current менше to, повертаємо об'єкт з властивістю "value",
+      // що містить поточне значення ,та не забуваємо збільшити індекс за допомогою інкремент, і "done" - false, означаючи, що ітерація ще не закінчена
+      return { value: this.current++, done: false };
+    } else {
+      // Якщо індекс вийшов за межі масиву ключів, повертаємо об'єкт з властивістю "done" - true, означаючи, що ітерація закінчена
+      return { done: true };
+    }
+  },
+};
+
+// Функція "useSymbolIterator" використовує ітератор для отримання значень об'єкта
+function useSymbolIterator() {
+  let result = []; // Створюємо масив для зберігання значень
+
+  // Проходимо крізь елементи об'єкта, використовуючи цикл "for...of"
+  for (let value of myObject) {
+    // Додаємо кожне значення до масиву "result"
+    result = [...result, value];
+  }
+
+  // Повертаємо масив зі значеннями
+  return result;
 }
 
 console.log("Завдання 10 ====================================");
-console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Виведе "42.99"
 
-// Задача 11: Напишіть функцію, яка конвертує число в рядок
-
-function convertToString(num) {
-  // Використовуємо метод toString(), щоб конвертувати число в рядок
-  return num.toString();
-}
-
-console.log("Завдання 11 ====================================");
-console.log("convertToString(42)", convertToString(42)); // Виведе "42"
-
-// Задача 12: Напишіть функцію, яка конвертує число в рядок з вказаною загальною кількістю значущих цифр
-
-function convertToPrecision(num, precision) {
-  // Використовуємо метод toPrecision(), щоб конвертувати число в рядок з вказаною загальною кількістю значущих цифр
-  return num.toPrecision(precision);
-}
-
-console.log("Завдання 12 ====================================");
-console.log("convertToPrecision(42.9876, 2)", convertToPrecision(42.9876, 2)); // Виведе "43"
+console.log(useSymbolIterator()); //Виведе [ 1, 2, 3, 4, 5, 6 ]
