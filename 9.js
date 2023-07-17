2e4e05252de9d07f8c16165e3e2810c1be03a10a
CLII412 <sw412fr@gmail.com>
2023-07-17 22:05:18 +0300

    init

---
+++ 9.js
@@ -0,0 +1,23 @@
+console.log("Завдання: 9 ==============================");
+
+function task9() {
+  // Створюємо функцію fetchWithError, яка симулює помилку при запиті до сервера.
+  function fetchWithError() {
+    // Створюємо константу errorMessage в яку записуємо рядок "Помилка при з'єднанні з сервером"
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    // Використовуємо Promise.reject(errorMessage) для створення вже відхиленого промісу.
+    return Promise.reject(new Error(errorMessage));
+  }
+
+  // Викликаємо функцію fetchWithError
+
+  fetchWithError()
+    // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+
+    .then((data) => console.log(data))
+    // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+
+    .catch((error) => console.log(error));
+}
+
+task9(); // Виведе: Error: Помилка при з'єднанні з сервером
